4205bb10a4371d072fa9a3cf80fb2c36e89a6a9e
João Pedro <54006291+JoaoPedroli@users.noreply.github.com>
2024-04-23 15:53:22 -0300

    Update 102.c

--- 102.c
+++ 102.c
@@ -5,8 +5,7 @@ void main() {
   printf("Digite o valor de N: ");
   scanf("%d", &N);
   printf("Todos os divisores exatos de N:\n");
-  const int METADE_DE_N = N / 2;
-  for(int i = 1; i <= METADE_DE_N; ++i) {
+  for(int i = 1; i <= N; ++i) {
     if(N % i == 0) printf("%d ", i);
   }
   printf("\n");
